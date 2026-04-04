ca681032a6185fdfc2c2df5fa7e19a2381b61410
Truong Duc Khai <duckhai80@gmail.com>
2026-04-04 20:39:13 +0700

    Initing discount model

---
+++ src/models/discount.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { InferSchemaType, Schema } from "mongoose";
+
+const DOCUMENT_NAME = "Discount";
+const COLLECTION_NAME = "discounts";
+
+// Declare the Schema of the Mongo model
+const discountSchema = new mongoose.Schema(
+  {
+    shopId: { type: Schema.Types.ObjectId, ref: "Shop" },
+    name: { type: String, required: true },
+    description: { type: String },
+    type: {
+      type: String,
+      enum: ["fixedAmound", "percentage"],
+      required: true,
+      default: "fixedAmount",
+    }, // or percentage
+    value: { type: Number, required: true }, // 100000 or 10%
+    code: { type: String, required: true },
+
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+
+    maxUses: { type: Number, required: true }, // total uses
+    maxUsesPerUser: { type: Number, required: true }, // max uses per user
+    minOrderValue: { type: Number, required: true },
+    usesCount: { type: Number, required: true }, // current uses
+    usersUsed: { type: Array, default: [] }, // users who used
+
+    isActive: { type: Boolean, default: true },
+    appliesTo: { type: String, required: true, enum: ["all", "specific"] },
+    appliesToProduct: { type: Array, default: [] },
+  },
+  { collection: COLLECTION_NAME, timestamps: true },
+);
+
+export type Discount = InferSchemaType<typeof discountSchema>;
+
+//Export the model
+export default mongoose.model(DOCUMENT_NAME, discountSchema);
